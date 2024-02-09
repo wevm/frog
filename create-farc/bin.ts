b3ddbcb76ebc83ef966658f99d8258335d197791
moxey.eth <jakemoxey@gmail.com>
2024-02-09 15:18:59 +1100

    chore: rename

--- create-farc/bin.ts
+++ create-farc/bin.ts
@@ -6,7 +6,7 @@ import { type CreateParameters, create } from './create.js'
 const require = createRequire(import.meta.url)
 const pkg = require('../package.json')
 
-const cli = cac('create-vocs')
+const cli = cac('create-farc')
 
 cli.usage('[options]').option('-n, --name [name]', 'Name of project')
 
