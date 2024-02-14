75dd408cd333bb74ee4e6d9cdd362ce87427db02
moxey.eth <jakemoxey@gmail.com>
2024-02-15 07:51:32 +1100

    feat: shorten urls

--- src/utils/requestToContext.ts
+++ src/utils/requestToContext.ts
@@ -3,19 +3,18 @@ import type { FrameContext } from '../types.js'
 
 type RequestToContextReturnType = Pick<
   FrameContext,
-  'status' | 'trustedData' | 'untrustedData' | 'url'
+  'frameData' | 'status' | 'url'
 >
 
 export async function requestToContext(
   request: Context['req'],
 ): Promise<RequestToContextReturnType> {
-  const { trustedData, untrustedData } =
+  const { trustedData: _trustedData, untrustedData } =
     (await request.json().catch(() => {})) || {}
 
   return {
+    frameData: untrustedData,
     status: request.method === 'POST' ? 'response' : 'initial',
-    trustedData,
-    untrustedData,
     url: request.url,
   }
 }
