cdb446c5b487761f69098e18f4832378d0519630
moxey.eth <jakemoxey@gmail.com>
2024-02-21 07:06:15 +1100

    docs: scaffold sidebar

--- site/vocs.config.ts
+++ site/vocs.config.ts
@@ -7,5 +7,55 @@ export default defineConfig({
       text: 'Getting Started',
       link: '/getting-started',
     },
+    {
+      text: 'Concepts',
+      items: [
+        {
+          text: 'Structure & Routing',
+        },
+        {
+          text: 'Actions',
+        },
+        {
+          text: 'Images & Intents',
+        },
+        {
+          text: 'Browser Redirects',
+        },
+        {
+          text: 'State Management',
+        },
+      ],
+    },
+    {
+      text: 'Platforms',
+      items: [
+        {
+          text: 'Bun',
+        },
+        {
+          text: 'Cloudflare',
+        },
+        {
+          text: 'Next.js',
+        },
+        {
+          text: 'Node.js',
+        },
+        {
+          text: 'Vercel',
+        },
+      ],
+    },
+    {
+      text: 'Reference',
+      items: [
+        { text: 'Farc' },
+        { text: 'Farc.frame' },
+        { text: 'Farc.hono' },
+        { text: 'Farc.route' },
+        { text: 'Intents' },
+      ],
+    },
   ],
 })
