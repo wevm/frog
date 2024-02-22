51f67d09f3a81855bb0ffa6335699db6f6fb65c5
moxey.eth <jakemoxey@gmail.com>
2024-02-22 13:46:52 +1100

    fix: jsx

--- .scripts/postbuild.ts
+++ .scripts/postbuild.ts
@@ -1,4 +1,4 @@
-import { dirname, relative, resolve } from 'node:path'
+import { resolve } from 'node:path'
 import glob from 'fast-glob'
 import { copy, rename } from 'fs-extra'
 
@@ -9,21 +9,11 @@ async function rewriteHonoJsx() {
   const files = await glob('./src/_lib/**/*.js')
   for (const file of files) {
     const content = await Bun.file(file).text()
-    const jsxDir = relative(
-      dirname(file),
-      resolve(import.meta.dirname, '../src/_lib/jsx'),
-    )
     await Bun.write(
       file,
       content
-        .replaceAll(
-          'hono/jsx/jsx-runtime',
-          `${jsxDir.startsWith('.') ? jsxDir : `./${jsxDir}`}/jsx-runtime`,
-        )
-        .replaceAll(
-          'hono/jsx/jsx-dev-runtime',
-          `${jsxDir.startsWith('.') ? jsxDir : `./${jsxDir}`}/jsx-dev-runtime`,
-        ),
+        .replaceAll('hono/jsx/jsx-runtime', 'frog/jsx/jsx-runtime')
+        .replaceAll('hono/jsx/jsx-dev-runtime', 'frog/jsx/jsx-dev-runtime'),
     )
   }
 }
