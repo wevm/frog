8b00e2060bfd9afa2664a95f8515f170f6ab8636
Vladyslav Dalechyn <vlad.dalechin@gmail.com>
2024-02-29 22:40:28 +0200

    nit: add space between grouped exports
    Co-authored-by: awkweb <tom@meagher.co>

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ export {
   type ButtonResetProps,
 } from './components/Button.js'
 export { TextInput, type TextInputProps } from './components/TextInput.js'
+
 export { extractFrameMetaTags } from './utils/extractFrameMetaTags.js'
 
 export { Frog } from './frog.js'
