0b8dcef1880f53febdfe728a0f6d536fe8cf3245
moxey.eth <jakemoxey@gmail.com>
2024-03-04 10:08:59 +1100

    tests: update

--- src/next/getFrameMetadata.test.ts
+++ src/next/getFrameMetadata.test.ts
@@ -2,12 +2,12 @@ import { expect, test } from 'vitest'
 import { getFrameMetadata } from './getFrameMetadata.js'
 
 test('default', async () => {
-  const metadata = await getFrameMetadata('https://frog.fm')
+  const metadata = await getFrameMetadata('https://frame.frog.fm/api')
   expect(metadata).toMatchInlineSnapshot(`
     {
       "fc:frame": "vNext",
       "fc:frame:button:1:action": "post",
-      "fc:frame:button:1:target": "https://frame.frog.fm/api/features",
+      "fc:frame:button:1:target": "https://frame.frog.fm/api/features?initialPath=%252Fapi&amp;previousButtonValues=%2523A_%252C_l%252C_l",
       "fc:frame:button:2": "Docs",
       "fc:frame:button:2:action": "link",
       "fc:frame:button:2:target": "https://frog.fm",
@@ -16,8 +16,8 @@ test('default', async () => {
       "fc:frame:button:3:target": "https://github.com/wevm/frog",
       "fc:frame:image": "https://frame.frog.fm/og.png",
       "fc:frame:image:aspect_ratio": "1.91:1",
-      "fc:frame:post_url": "https://frame.frog.fm/api",
-      "fc:frame:state": "%7B%22initialPath%22%3A%22%2Fapi%22%2C%22previousButtonValues%22%3A%5Bnull%2C%22_l%22%2C%22_l%22%5D%2C%22previousState%22%3A%7B%22featureIndex%22%3A0%7D%7D",
+      "fc:frame:post_url": "https://frame.frog.fm/api?initialPath=%252Fapi&amp;previousButtonValues=%2523A_%252C_l%252C_l",
+      "frog:version": "0.2.10",
     }
   `)
 })
--- src/utils/getFrameMetadata.test.ts
+++ src/utils/getFrameMetadata.test.ts
@@ -2,7 +2,7 @@ import { expect, test } from 'vitest'
 import { getFrameMetadata } from './getFrameMetadata.js'
 
 test('default', async () => {
-  const metadata = await getFrameMetadata('https://frog.fm')
+  const metadata = await getFrameMetadata('https://frame.frog.fm/api')
   expect(metadata).toMatchInlineSnapshot(`
     [
       {
@@ -18,19 +18,15 @@ test('default', async () => {
         "property": "fc:frame:image",
       },
       {
-        "content": "https://frame.frog.fm/api",
+        "content": "https://frame.frog.fm/api?initialPath=%252Fapi&amp;previousButtonValues=%2523A_%252C_l%252C_l",
         "property": "fc:frame:post_url",
-      },
-      {
-        "content": "%7B%22initialPath%22%3A%22%2Fapi%22%2C%22previousButtonValues%22%3A%5Bnull%2C%22_l%22%2C%22_l%22%5D%2C%22previousState%22%3A%7B%22featureIndex%22%3A0%7D%7D",
-        "property": "fc:frame:state",
       },
       {
         "content": "post",
         "property": "fc:frame:button:1:action",
       },
       {
-        "content": "https://frame.frog.fm/api/features",
+        "content": "https://frame.frog.fm/api/features?initialPath=%252Fapi&amp;previousButtonValues=%2523A_%252C_l%252C_l",
         "property": "fc:frame:button:1:target",
       },
       {
@@ -56,6 +52,10 @@ test('default', async () => {
       {
         "content": "https://github.com/wevm/frog",
         "property": "fc:frame:button:3:target",
+      },
+      {
+        "content": "0.2.10",
+        "property": "frog:version",
       },
     ]
   `)
