1980abc08bbb1d793d05c077525ec5a8e1197686
moxey.eth <jakemoxey@gmail.com>
2024-03-06 22:39:38 +1100

    docs

--- playground/src/middleware.tsx
+++ playground/src/middleware.tsx
@@ -1,6 +1,8 @@
 import { Frog } from 'frog'
 import type { MiddlewareHandler } from 'hono'
 
+import { neynarMiddleware } from './neynar'
+
 type EchoMiddlewareVariables = {
   echo: (str: string) => string
 }
@@ -23,6 +25,16 @@ app.frame('/', (c) => {
     image: (
       <div style={{ color: 'white', display: 'flex', fontSize: 60 }}>
         {c.var.echo('hello world!')}
+      </div>
+    ),
+  })
+})
+
+app.frame('/neynar', neynarMiddleware, (c) => {
+  return c.res({
+    image: (
+      <div style={{ color: 'white', display: 'flex', fontSize: 60 }}>
+        {c.var.interactor?.displayName}
       </div>
     ),
   })
