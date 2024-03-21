9b36a313f1196b946bb90e2db23c9bac0ef81d43
moxey.eth <jakemoxey@gmail.com>
2024-03-21 13:24:30 +1100

    tweak

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,8 +1,2 @@
-export { Box, type BoxProps } from './Box.js'
-export { Cover, type CoverProps } from './Cover.js'
 export { createSystem } from './createSystem.js'
-export { VStack, type VStackProps } from './VStack.js'
-export {
-  type Tokens,
-  colors,
-} from './tokens.js'
+export { colors } from './tokens.js'
