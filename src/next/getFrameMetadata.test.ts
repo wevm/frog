047c6467cf152da92c30ffefa3076da2ce88836e
Tom Meagher <tom@meagher.co>
2024-03-28 10:24:41 -0400

    chore: snaps

--- src/next/getFrameMetadata.test.ts
+++ src/next/getFrameMetadata.test.ts
@@ -10,7 +10,7 @@ test('default', async () => {
       "fc:frame": "vNext",
       "fc:frame:button:1": "Features →",
       "fc:frame:button:1:action": "post",
-      "fc:frame:button:1:target": "https://frame.frog.fm/api/features?initialPath=%252Fapi&amp;previousButtonValues=%2523A_%252C_l%252C_l",
+      "fc:frame:button:1:post_url": "https://frame.frog.fm/api/features?initialPath=%252Fapi&amp;previousButtonValues=%2523A_%252C_l%252C_l",
       "fc:frame:button:2": "Docs",
       "fc:frame:button:2:action": "link",
       "fc:frame:button:2:target": "https://frog.fm",
--- src/utils/getFrameMetadata.test.ts
+++ src/utils/getFrameMetadata.test.ts
@@ -41,7 +41,7 @@ test('default', async () => {
       },
       {
         "content": "https://frame.frog.fm/api/features?initialPath=%252Fapi&amp;previousButtonValues=%2523A_%252C_l%252C_l",
-        "property": "fc:frame:button:1:target",
+        "property": "fc:frame:button:1:post_url",
       },
       {
         "content": "Docs",
