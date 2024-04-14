wevm/frog

commit e38c8986a8ec5af84ea80fee3fcdf8e078996e0b

fix: don't delete port if forwarded host is localhost (#254)
* fix: don't delete port if forwarded host is localhost

Fixes #253

* chore: changesets

src/utils/getRequestUrl.ts
@@ -5,6 +5,6 @@ export function getRequestUrl(req: Context['req']) {
   const forwardedHost = req.header('x-forwarded-host')
   url.host = forwardedHost ?? url.host
   url.protocol = req.header('x-forwarded-proto') ?? url.protocol
-  if (forwardedHost) url.port = ''
+  if (!forwardedHost?.startsWith('localhost')) url.port = ''
   return url
 }

commit 3fae92d38d4820a319358eb161df30407e2299c4

fix: don't delete port if there is no forwarded host
A regression came in e38c898 that fixed the local devtools with NextJS,
but ultimately didn't check if `X-Forwarded-Host` was even present.

src/utils/getRequestUrl.ts
@@ -5,6 +5,7 @@ export function getRequestUrl(req: Context['req']) {
   const forwardedHost = req.header('x-forwarded-host')
   url.host = forwardedHost ?? url.host
   url.protocol = req.header('x-forwarded-proto') ?? url.protocol
-  if (!forwardedHost?.startsWith('localhost')) url.port = ''
+  if (forwardedHost !== undefined && !forwardedHost.startsWith('localhost'))
+    url.port = ''
   return url
 }