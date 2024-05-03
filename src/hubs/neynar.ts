001705270445172cbfa17fbf3e1b82da2d102743
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2024-05-03 22:55:53 +0530

    feat: add frame validate using neynar for neynar hubs (#292)
    * feat: add frame validate using neynar for neynar hubs
    
    * fix: remove unnecessary async
    
    * feat: use hub api key instead
    
    * refactor: improve from feedback
    
    * chore: remove debugging console log
    
    * fix: comment;
    
    * chore: format
    
    * chore: changeset
    
    ---------
    
    Co-authored-by: Tom Meagher <tom@meagher.co>

--- src/hubs/neynar.ts
+++ src/hubs/neynar.ts
@@ -1,3 +1,4 @@
+import type { TrustedData } from '../types/frame.js'
 import { createHub } from './utils.js'
 
 export type NeynarHubParameters = {
@@ -6,12 +7,26 @@ export type NeynarHubParameters = {
 
 export const neynar = createHub((parameters: NeynarHubParameters) => {
   const { apiKey } = parameters
+
   return {
     apiUrl: 'https://hub-api.neynar.com',
     fetchOptions: {
       headers: {
         api_key: apiKey,
       },
+    },
+    verifyFrame: async ({ trustedData }: { trustedData: TrustedData }) => {
+      return await fetch('https://api.neynar.com/v2/farcaster/frame/validate', {
+        method: 'POST',
+        headers: {
+          accept: 'application json',
+          api_key: apiKey,
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          message_bytes_in_hex: `0x${trustedData.messageBytes}`,
+        }),
+      }).then(async (res) => res.json())
     },
   }
 })
--- src/types/hub.ts
+++ src/types/hub.ts
@@ -1,6 +1,10 @@
+import type { TrustedData } from './frame.js'
+
 export type Hub = {
   /** Hub API URL. */
   apiUrl: string
   /** Options to pass to `fetch`. */
   fetchOptions?: RequestInit
+  /** Verify frame override. */
+  verifyFrame?: (parameters: { trustedData: TrustedData }) => Promise<void>
 }
