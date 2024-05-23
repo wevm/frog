9e998cd34a7e0555cd57b76bedc1f3d8a19d8240
hotwater <vladyslav@spilnota.xyz>
2024-05-23 14:16:22 +0300

    docs: up

--- src/frog-base.tsx
+++ src/frog-base.tsx
@@ -189,9 +189,33 @@ export type RouteOptions<method extends string = string> = Pick<
       }
     : method extends 'castAction'
       ? {
+          /**
+           * An action name up to 30 characters.
+           *
+           * @example `'My action.'`
+           */
           name: string
+          /**
+           * An icon ID.
+           *
+           * @see https://warpcast.notion.site/Spec-Farcaster-Actions-84d5a85d479a43139ea883f6823d8caa
+           * @example `'log'`
+           */
           icon: Octicon
+          /**
+           * A short description up to 80 characters.
+           *
+           * @example `'My awesome action description.'`
+           */
           description?: string
+          /**
+           * Optional external link to an "about" page.
+           * You should only include this if you can't fully describe your
+           * action using the `description` field.
+           * Must be http or https protocol.
+           *
+           * @example `'My awesome action description.'`
+           */
           aboutUrl?: string
         }
       : {})
