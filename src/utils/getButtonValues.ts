90544316bf2d752bf324bc43662cd31b3b2c78db
Vladyslav Dalechyn <vlad.dalechin@gmail.com>
2024-07-25 23:27:49 +0300

    fix: check for `prop` in `intent` (#444)
    This is quite a mysterious bug since it cannot be reproduced locally,
    and is only reproduced on Vercel/Next.js Platforms.
    I assume that some sort of build caching is being made and previous
    fixes didn't come through, thus trying now with this one.

--- src/utils/getButtonValues.ts
+++ src/utils/getButtonValues.ts
@@ -8,7 +8,7 @@ export function getButtonValues(
 
   const buttonValues: FrameButtonValue[] = []
   for (const intent of intents) {
-    if (!intent) continue
+    if (!intent || !('props' in intent)) continue
     const { property } = intent.props
     if (!(property as string).match(/^fc:frame:button:(1|2|3|4)$/)) continue
     buttonValues.push(intent.props['data-value'])
