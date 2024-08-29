6df63fee5387438a41d98fc24f8f7e558f878575
dalechyn <vladyslav@spilnota.xyz>
2024-08-29 18:06:53 +0300

    test: fix

--- src/next/getFrameMetadata.test.ts
+++ src/next/getFrameMetadata.test.ts
@@ -10,7 +10,7 @@ test('default', async () => {
       "fc:frame": "vNext",
       "fc:frame:button:1": "Features →",
       "fc:frame:button:1:action": "post",
-      "fc:frame:button:1:target": "https://frame.frog.fm/api/features?initialPath=%252Fapi&amp;previousButtonValues=%2523A_%252C_l%252C_l",
+      "fc:frame:button:1:target": "https://frame.frog.fm/api/features?initialPath=%252Fapi&previousButtonValues=%2523A_%252C_l%252C_l",
       "fc:frame:button:2": "Docs",
       "fc:frame:button:2:action": "link",
       "fc:frame:button:2:target": "https://frog.fm",
@@ -19,7 +19,7 @@ test('default', async () => {
       "fc:frame:button:3:target": "https://github.com/wevm/frog",
       "fc:frame:image": "https://frame.frog.fm/og.png",
       "fc:frame:image:aspect_ratio": "1.91:1",
-      "fc:frame:post_url": "https://frame.frog.fm/api?initialPath=%252Fapi&amp;previousButtonValues=%2523A_%252C_l%252C_l",
+      "fc:frame:post_url": "https://frame.frog.fm/api?initialPath=%252Fapi&previousButtonValues=%2523A_%252C_l%252C_l",
       "og:image": "https://frame.frog.fm/og.png",
       "og:title": "Frog – Framework for Farcaster Frames",
     }
