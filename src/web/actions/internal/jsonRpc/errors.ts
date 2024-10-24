e99f948a94a9042cdec2cfa0d59f5e48e6d7344d
dalechyn <vladyslav@spilnota.xyz>
2024-10-24 21:51:39 +0300

    nit: better error

--- src/web/actions/internal/jsonRpc/errors.ts
+++ src/web/actions/internal/jsonRpc/errors.ts
@@ -1,6 +1,12 @@
 export class JsonRpcError extends Error {
+  code: number
+  requestId: string
   constructor(requestId: string, code: number, message: string) {
-    super(`Request ${requestId} failed with code ${code} – ${message}`)
+    super(`Request ${requestId} failed with code ${code}`, {
+      cause: message,
+    })
     this.name = 'JsonRpcError'
+    this.code = code
+    this.requestId = requestId
   }
 }
